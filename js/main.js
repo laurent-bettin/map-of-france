54efc118754fabcb000c20e4736e11a626dbc68b
laurent-bettin <b.rhaze@gmail.com>
2014-02-24 22:41:36 +0100

    better encapsulation

--- js/main.js
+++ js/main.js
@@ -1,168 +1,173 @@
 var front = {};
 
-function isFunction(functionToCheck) {
-    var getType = {};
-    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-}
+var mapOfFrance = (function() {
 
-var mapOfFrance = function(canvasId, width, height) {
-    this.paper = new Raphael(canvasId, width, height);
-    this.fset = null;
-    this.opts = {
-        transformationMatrix: 'm1 0 0 1 0 0',
-        regionsOpts: {
-            // possible attributs : http://raphaeljs.com/reference.html#Element.attr
-            attr: {
-                base: {
-                    'stroke': '#666'
+    //private static method
+    var isFunction = function(functionToCheck) {
+        var getType = {};
+        return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+    }
+
+    var construct = function(canvasId, width, height) {
+        this.paper = new Raphael(canvasId, width, height);
+        this.fset = null;
+        this.opts = {
+            transformationMatrix: 'm1 0 0 1 0 0',
+            regionsOpts: {
+                // possible attributs : http://raphaeljs.com/reference.html#Element.attr
+                attr: {
+                    base: {
+                        'stroke': '#666'
+                    }
+                },
+                exclude: {
+                    all: false,
+                    list: []
                 }
             },
-            exclude: {
-                all: false,
-                list: []
+            deptOpts: {
+                // possible attributs : http://raphaeljs.com/reference.html#Element.attr
+                attr: {
+                    base: {
+                        'fill': '#d9dadb',
+                        'stroke': '#888',
+                        'cursor': 'pointer'
+                    }
+                },
+                exclude: {
+                    all: false,
+                    list: []
+                },
+                onMouseOver: null,
+                onMouseOut: null
             }
-        },
-        deptOpts: {
-            // possible attributs : http://raphaeljs.com/reference.html#Element.attr
-            attr: {
-                base: {
-                    'fill': '#d9dadb',
-                    'stroke': '#888',
-                    'cursor': 'pointer'
-                }
-            },
-            exclude: {
-                all: false,
-                list: []
-            },
-            onMouseOver: null,
-            onMouseOut: null
-        }
+        };
+
     };
 
-    return this;
-
-};
-
-//return raphael instance
-mapOfFrance.prototype.getPaper = function() {
-    return this.paper;
-}
-
-mapOfFrance.prototype.setTransformationMatrix = function (transformationMatrix) {
-    this['opts']['transformationMatrix'] = transformationMatrix;
-    return this;
-}
-
-//change default attribut for departments
-//chainable
-mapOfFrance.prototype.setDeptBaseAttr = function(attr) {
-    this['opts']['deptOpts']['attr']['base'] = attr;
-    return this;
-}
-
-//return default attributes of departements
-mapOfFrance.prototype.getDeptBaseAttr = function() {
-    return this['opts']['deptOpts']['attr']['base'];
-}
-
-//change default attribut for regions
-//chainable
-mapOfFrance.prototype.setRegionsBaseAttr = function(attr) {
-    this['opts']['regionsOpts']['attr']['base'] = attr;
-    return this;
-}
-
-//return default attributes of regions
-mapOfFrance.prototype.getRegionsBaseAttr = function() {
-    return this['opts']['regionsOpts']['attr']['base'];
-}
-
-//exclude all regions from rendering
-//chainable
-mapOfFrance.prototype.excludeRegions = function() {
-    this['opts']['regionsOpts']['exclude']['all'] = true;
-    return this;
-}
-
-//exclude all departments from rendering
-//chainable
-mapOfFrance.prototype.excludeDepts = function() {
-    this['opts']['deptOpts']['exclude']['all'] = true;
-    return this;
-}
-
-//Set a custom list of departments to exclude from rendering
-//chainable
-mapOfFrance.prototype.setDeptsExcludeList = function(excludeList) {
-    this['opts']['deptOpts']['exclude']['list'] = excludeList;
-    return this;
-}
-
-//Set a custom list of regions to exclude from rendering
-//chainable
-mapOfFrance.prototype.setRegionsExcludeList = function(excludeList) {
-    this['opts']['regionsOpts']['exclude']['list'] = excludeList;
-    return this;
-}
-
-mapOfFrance.prototype.createPath = function(coo, attr, datas) {
-    var path = this.paper.path(coo);
-    path.attr(attr);
-    for (var k in datas) {
-        path.data(k, datas[k])
+    //return raphael instance
+    construct.prototype.getPaper = function() {
+        return this.paper;
     }
-    return path;
-};
 
-mapOfFrance.prototype.draw = function() {
+    construct.prototype.setTransformationMatrix = function (transformationMatrix) {
+        this['opts']['transformationMatrix'] = transformationMatrix;
+        return this;
+    }
 
-    var departments = franceData['departments']; //shortcut
-    var regions = franceData['regions']; //shortcut
-    var deptOpts = this['opts']['deptOpts'];
-    var regionOpts = this['opts']['regionsOpts'];
-    var node;
-    var datas;
+    //change default attribut for departments
+    //chainable
+    construct.prototype.setDeptBaseAttr = function(attr) {
+        this['opts']['deptOpts']['attr']['base'] = attr;
+        return this;
+    }
 
-    this.paper.setStart();
+    //return default attributes of departements
+    construct.prototype.getDeptBaseAttr = function() {
+        return this['opts']['deptOpts']['attr']['base'];
+    }
 
-    if (!deptOpts['exclude']['all']) {
-        for (var d in departments) {
-            if (deptOpts['exclude']['list'].indexOf(d) === -1) {
-                // var newPath = Raphael.transformPath(departments[d]['coo'], transformationMatrix)
-                datas = {
-                    'data-name': departments[d]['name'],
-                    'data-insee': d
+    //change default attribut for regions
+    //chainable
+    construct.prototype.setRegionsBaseAttr = function(attr) {
+        this['opts']['regionsOpts']['attr']['base'] = attr;
+        return this;
+    }
+
+    //return default attributes of regions
+    construct.prototype.getRegionsBaseAttr = function() {
+        return this['opts']['regionsOpts']['attr']['base'];
+    }
+
+    //exclude all regions from rendering
+    //chainable
+    construct.prototype.excludeRegions = function() {
+        this['opts']['regionsOpts']['exclude']['all'] = true;
+        return this;
+    }
+
+    //exclude all departments from rendering
+    //chainable
+    construct.prototype.excludeDepts = function() {
+        this['opts']['deptOpts']['exclude']['all'] = true;
+        return this;
+    }
+
+    //Set a custom list of departments to exclude from rendering
+    //chainable
+    construct.prototype.setDeptsExcludeList = function(excludeList) {
+        this['opts']['deptOpts']['exclude']['list'] = excludeList;
+        return this;
+    }
+
+    //Set a custom list of regions to exclude from rendering
+    //chainable
+    construct.prototype.setRegionsExcludeList = function(excludeList) {
+        this['opts']['regionsOpts']['exclude']['list'] = excludeList;
+        return this;
+    }
+
+    construct.prototype.createPath = function(coo, attr, datas) {
+        var path = this.paper.path(coo);
+        path.attr(attr);
+        for (var k in datas) {
+            path.data(k, datas[k])
+        }
+        return path;
+    };
+
+    construct.prototype.draw = function() {
+
+        var departments = franceData['departments']; //shortcut
+        var regions = franceData['regions']; //shortcut
+        var deptOpts = this['opts']['deptOpts'];
+        var regionOpts = this['opts']['regionsOpts'];
+        var node;
+        var datas;
+
+        this.paper.setStart();
+
+        if (!deptOpts['exclude']['all']) {
+            for (var d in departments) {
+                if (deptOpts['exclude']['list'].indexOf(d) === -1) {
+                    // var newPath = Raphael.transformPath(departments[d]['coo'], transformationMatrix)
+                    datas = {
+                        'data-name': departments[d]['name'],
+                        'data-insee': d
+                    }
+                    node = this.createPath(departments[d]['coo'], deptOpts['attr']['base'], datas)
+                    if (isFunction(deptOpts['onMouseOver'])) {
+                        node.mouseover(deptOpts['onMouseOver']);
+                    }
+                    if (isFunction(deptOpts['onMouseOut'])) {
+                        node.mouseout(deptOpts['onMouseOut']);
+                    }
+                    // node.hover(deptOpts['onMouseOver'], deptOpts['onMouseOut']);
                 }
-                node = this.createPath(departments[d]['coo'], deptOpts['attr']['base'], datas)
-                if (isFunction(deptOpts['onMouseOver'])) {
-                    node.mouseover(deptOpts['onMouseOver']);
-                }
-                if (isFunction(deptOpts['onMouseOut'])) {
-                    node.mouseout(deptOpts['onMouseOut']);
-                }
-                // node.hover(deptOpts['onMouseOver'], deptOpts['onMouseOut']);
             }
         }
-    }
 
-    if (!regionOpts['exclude']['all']) {
-        for (var r in regions) {
-            if (regionOpts['exclude']['list'].indexOf(r) === -1) {
-                datas = {
-                    'data-name': regions[r]['name'],
-                    'data-insee': r
+        if (!regionOpts['exclude']['all']) {
+            for (var r in regions) {
+                if (regionOpts['exclude']['list'].indexOf(r) === -1) {
+                    datas = {
+                        'data-name': regions[r]['name'],
+                        'data-insee': r
+                    }
+                    node = this.createPath(regions[r]['coo'], regionOpts['attr']['base'], datas)
+                    // node.hover(mapOfFrance.regionMouseOver, mapOfFrance.regionMouseOver);
+                    // var newPath = Raphael.transformPath(regions[r]['coo'], transformationMatrix)
                 }
-                node = this.createPath(regions[r]['coo'], regionOpts['attr']['base'], datas)
-                // node.hover(mapOfFrance.regionMouseOver, mapOfFrance.regionMouseOver);
-                // var newPath = Raphael.transformPath(regions[r]['coo'], transformationMatrix)
             }
         }
-    }
 
-    this.fset = this.paper.setFinish();
-    this.fset.transform(this['opts']['transformationMatrix']);
-};
+        this.fset = this.paper.setFinish();
+        this.fset.transform(this['opts']['transformationMatrix']);
+    };
+
+    return construct;
+
+})();
 
 (function (context) {
     "use strict";
